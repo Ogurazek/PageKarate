b68a9556f39bb67cacac6d66e8d3ea4c06fdf8d4
Perez Elías <eg378556@gmail.com>
2025-11-02 08:12:44 -0300

    Mejorando la estructura y animaciones de la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,62 +15,66 @@ export default function Home() {
     <>
       <title>Karate Do / Inicio</title>
       <article className={styles.containerHome}>
-        <section className={styles.containerTextHome}>
-          <div>
-            <h1> Karate Do para la Mente, Cuerpo y Espíritu </h1>
-          </div>
-          <div>
-            <p>Somos una escuela de Karate Do Tradicional Kyudokan Shorin Ryu y Kobudo Ryukonkai, con sede en Chaco, Argentina. Nos especializamos en defensa personal (combate de manos vacías) y competencias deportivas, tanto individuales como en equipo, en niveles provinciales, nacionales e internacionales</p>
-          </div>
-          <section className={styles.containerJoinToUs}>
-            <span>¿Qué estas esperando para unirte?</span>
-            <Link href="/Inscripciones"><button>Unirme ahora</button></Link>
+        <div className={styles.containerTextImage}>
+          <section className={styles.containerTextHome}>
+            <div>
+              <h1> Karate Do para la Mente, Cuerpo y Espíritu </h1>
+            </div>
+            <div>
+              <p>Somos una escuela de Karate Do Tradicional Kyudokan Shorin Ryu y Kobudo Ryukonkai, con sede en Chaco, Argentina. Nos especializamos en defensa personal (combate de manos vacías) y competencias deportivas, tanto individuales como en equipo, en niveles provinciales, nacionales e internacionales</p>
+            </div>
+            <section className={styles.containerJoinToUs}>
+              <span>¿Qué estas esperando para unirte?</span>
+              <Link href="/Inscripciones"><button>Unirme ahora</button></Link>
+            </section>
           </section>
-        </section>
-        <div className={styles.containerImgLogoHome}>
-          <div>
-            <Image src="/LogoDojohd2.png" alt="Logo Okinawa" width={400} height={300} style={{ width: '100%', height: '100%' }} />
+          <div className={styles.containerImgLogoHome}>
+            <div>
+              <Image src="/LogoDojohd2.png" alt="Logo Okinawa" width={400} height={300} style={{ width: '100%', height: '100%' }} />
+            </div>
           </div>
         </div>
       </article>
       <article className={styles.containerHome2}>
-        <section className={styles.containerMiniCards}>
-          <p>Ofrecemos Clases Para</p>
-          <div className={styles.containerMiniCards2}>
-            <MiniCards title='Niños' />
-            <MiniCards title='Adolescentes' />
-            <MiniCards title='Adultos' />
-            <MiniCards title='Familias' />
-          </div>
-        </section>
-        <section className={styles.containerCard}>
-          <div className={styles.containerSecondImg}>
-            <div className={styles.recuadroImg}>
-              <div className={styles.contenedorImg}>
-                <Fade direction="left" triggerOnce>
-                  <Image src="/fotoportada.webp" alt="prueba" width={400} height={400} />
-                </Fade>
-              </div>
-              <div className={styles.contenedorTexto}>
-                <p>Cada fin de año, nuestros alumnos rinden examen en nuestra Sede Central en Corrientes (Capital), el Dojo Shibana, para demostrar lo aprendido a lo largo del año. La evaluación está a cargo del Director Técnico de la Región Norte, Sensei Moratto Hugo Mariano, 8° Dan Kyoshi.       <a href="/Nosotros">Ver más</a>
-                </p>
+        <div className={styles.containerMini}>
+          <section className={styles.containerMiniCards}>
+            <p>Ofrecemos Clases Para</p>
+            <div className={styles.containerMiniCards2}>
+              <MiniCards title='Niños' />
+              <MiniCards title='Adolescentes' />
+              <MiniCards title='Adultos' />
+              <MiniCards title='Familias' />
+            </div>
+          </section>
+          <section className={styles.containerCard}>
+            <div className={styles.containerSecondImg}>
+              <div className={styles.recuadroImg}>
+                <div className={styles.contenedorImg}>
+                  <Fade direction="left" triggerOnce>
+                    <Image src="/fotoportada.webp" alt="prueba" width={400} height={400} />
+                  </Fade>
+                </div>
+                <div className={styles.contenedorTexto}>
+                  <p>Cada fin de año, nuestros alumnos rinden examen en nuestra Sede Central en Corrientes (Capital), el Dojo Shibana, para demostrar lo aprendido a lo largo del año. La evaluación está a cargo del Director Técnico de la Región Norte, Sensei Moratto Hugo Mariano, 8° Dan Kyoshi.       <a href="/Nosotros">Ver más</a>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <Fade direction="right" triggerOnce>
-            <div className={styles.containerSecondText}>
-              <div className={styles.recuadroTexto}>
-                <span>🏆 Okinawa Shorin Ryu Kyudokan</span>
-                <strong>空手道少林流弓道館</strong>
-                <p>El Shorin Ryu es uno de los primeros estilos de Karate que surgieron en Okinawa, fundado por Choshin Chibana (1885-1969). Su nombre significa "Estilo del Bosque Pequeño" y es una adaptación de las enseñanzas del antiguo Shuri-Te, el arte marcial practicado en la capital de Okinawa, Shuri.</p>
-                <p>El Kyudokan es una de las principales ramas del Shorin Ryu, fundada por Yuchoku Higa (1910-1994), un discípulo directo de Choshin Chibana. Su objetivo era preservar las técnicas tradicionales del Karate de Okinawa con un énfasis en la fluidez, la precisión y el control corporal.
-                  El nombre "Kyudokan" significa "Casa de la Investigación", reflejando el compromiso de Higa con el estudio profundo del Karate. En este estilo, se pone especial atención en la postura, la respiración y la aplicación práctica de las técnicas en combate real.
-                </p>
-                <Link href="/Nosotros">Ver más</Link>
+            <Fade direction="right" triggerOnce>
+              <div className={styles.containerSecondText}>
+                <div className={styles.recuadroTexto}>
+                  <span>🏆 Okinawa Shorin Ryu Kyudokan</span>
+                  <strong>空手道少林流弓道館</strong>
+                  <p>El Shorin Ryu es uno de los primeros estilos de Karate que surgieron en Okinawa, fundado por Choshin Chibana (1885-1969). Su nombre significa "Estilo del Bosque Pequeño" y es una adaptación de las enseñanzas del antiguo Shuri-Te, el arte marcial practicado en la capital de Okinawa, Shuri.</p>
+                  <p>El Kyudokan es una de las principales ramas del Shorin Ryu, fundada por Yuchoku Higa (1910-1994), un discípulo directo de Choshin Chibana. Su objetivo era preservar las técnicas tradicionales del Karate de Okinawa con un énfasis en la fluidez, la precisión y el control corporal.
+                    El nombre "Kyudokan" significa "Casa de la Investigación", reflejando el compromiso de Higa con el estudio profundo del Karate. En este estilo, se pone especial atención en la postura, la respiración y la aplicación práctica de las técnicas en combate real.
+                  </p>
+                  <Link href="/Nosotros">Ver más</Link>
+                </div>
               </div>
-            </div>
-          </Fade>
-        </section>
+            </Fade>
+          </section>
+        </div>
       </article>
       <article className={styles.containerHome3}>
         <div className={styles.containerSubTitle}>
